d6bc55d720f98a41eb540c349f81be33182e222d
imtiazsamoo-rgb <imtiaz.samoo@gmail.com>
2026-06-27 12:25:21 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bfs-attendance-v1';
+const CACHE_NAME = 'bfs-attendance-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -14,9 +14,24 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
